a0f9110b4b5bc985efb15e62a510d70143f0c3bb
amitm <akharadi.meena67@gmail.com>
2019-09-23 11:55:08 +0530

    code updated

--- src/index.ts
+++ src/index.ts
@@ -1,61 +1,76 @@
-import * as functions from 'firebase-functions';
-import * as storage from '@google-cloud/storage';
+import * as functions from "firebase-functions";
+import { tmpdir } from "os";
+import { join, dirname } from "path";
+import * as storage from "@google-cloud/storage";
 const gcs = new storage.Storage();
-import { tmpdir } from 'os';
-import { join, dirname } from 'path';
-require('typescript-require');
-// Get functions.ts
-const sharp = require('sharp');
-import * as fs from 'fs-extra';
-import * as admin from 'firebase-admin';
-const firebase= admin.initializeApp(functions.config().firebase); 
-exports.generateThumbnail =  functions.storage.object().onFinalize( async (object) => {
-    console.log('start');
-    const fileBucket = gcs.bucket(object.bucket); // The Storage bucket that contains the file.
-    const filePath: any =  object.name; // File path in the bucket.
-    const fileName = filePath.split('/').pop();
-    const bucketDir = dirname(filePath);
-    const workingDir = join(tmpdir(), 'thumbs');
-    const tempFilePath = join(workingDir, `abc${fileName}`);
-    if ( fileName.startsWith('thumb@') ||
-      !String(object.contentType).startsWith('image/')
-    ) {
-       console.log(`Already a Thumbnail. or file is not valid`);
-      return;
-    }
-    console.log(workingDir);
-    await fs.ensureDir(workingDir);
-    const thumbFileName = fileName !== `thumb@_${fileName}` ? `thumb@_${fileName}` : fileName;
-    const thumbFilePath = join(workingDir, thumbFileName);
-    console.log(thumbFilePath);
-    await fileBucket.file(filePath).download({
-      destination: tempFilePath
-    }).then( async () => {
-      await sharp(tempFilePath).resize(100, 100).toFile(thumbFilePath);
-      console.log(thumbFilePath);
-    }).then(async ()=> {
-      await fileBucket.upload(thumbFilePath, {
-        destination: join(bucketDir, thumbFileName)
-      });
-    }).then(async ()=>{
-      console.log('done');
-      return fs.remove(workingDir).then(()=>{
-          return fs.remove(bucketDir);
-      });
-    });
-  });
+require("typescript-require");
+const sharp = require("sharp");
+import * as fs from "fs-extra";
+import * as admin from "firebase-admin";
+const firebase = admin.initializeApp(functions.config().firebase);
+const bucketName = "testthumbnail-991fe.appspot.com";
+exports.generateThumbnail = functions.https.onRequest(async () => {
+  const bucket = gcs.bucket(bucketName);
+  await bucket.getFiles().then(async (result: any) => {
+    // tslint:disable-next-line:prefer-for-of
+    for (let index = 0; index < result.length; index++) {
+      result[index].forEach(async (image:any) => {
+      const filePath: any = image.name; // File path in the bucket.
+      const fileName = filePath.split("/").pop();
+      const bucketDir = dirname(filePath);
+      const workingDir = join(tmpdir(), "thumbs");
 
-
-  exports.deleteImages = functions.https.onRequest(()=> {
-        const bucket = firebase.storage().bucket('testthumbnail-991fe.appspot.com');
-        return bucket.deleteFiles({
-            prefix: `thumb@`
+      const tempFilePath = join(workingDir, `${fileName}`);
+      if (
+        fileName.startsWith("thumb@") ||
+        !fileName.match(/.(jpg|jpeg|png|gif)$/i)
+      ) {
+        console.log(`Already a Thumbnail. or file is not valid`);
+        return;
+      }
+      console.log(workingDir);
+      await fs.ensureDir(workingDir);
+      const thumbFileName =
+        fileName !== `thumb@_${fileName}` ? `thumb@_${fileName}` : fileName;
+      const thumbFilePath = join(workingDir, thumbFileName);
+      await bucket
+        .file(filePath)
+        .download({
+          destination: tempFilePath
+        })
+        .then(async () => {
+          await sharp(tempFilePath)
+            .resize(100, 100)
+            .toFile(thumbFilePath);
+          console.log(thumbFilePath);
+        })
+        .then(async () => {
+          await bucket.upload(thumbFilePath, {
+            destination: join(bucketDir, thumbFileName)
+          });
+        })
+        .then(() => {
+          console.log("done");
+          return fs.remove(workingDir);
         });
+        
       });
+    }
+    
+  
+  });
+});
 
-  // sheduler function for deleting files
+exports.deleteImages = functions.https.onRequest(() => {
+  const bucket = firebase.storage().bucket(bucketName);
+  return bucket.deleteFiles({
+    prefix: `thumb@`
+  });
+});
+
+// sheduler function for deleting files
 //   exports.deleteImages = functions.pubsub.schedule('1 11 * * *').onRun((context) => {
-//     const bucket = firebase.storage().bucket('testthumbnail-991fe.appspot.com');
+//     const bucket = firebase.storage().bucket(bucketName);
 //     return bucket.deleteFiles({
 //         prefix: `thumb@`
 //     });
